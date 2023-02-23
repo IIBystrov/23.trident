46f4adc6ed40324aa5e1677e518c8a501fffc9b8
IIBystrov <i.i.bystrov@yandex.ru>
2023-02-23 11:40:20 +0300

    Add script and styles for .carousel (style.css, app.js)

--- js/app.js
+++ js/app.js
@@ -21,8 +21,15 @@ const menu = document.querySelector(".header__menu");
 const slider = document.querySelector(".slider__list");
 const sliderItems = Array.from(slider.children);
 
+const carouselContain = document.querySelector(".main__carousel");
+const carousel = carouselContain.querySelector(".carousel__list");
+const carouselItems = Array.from(carousel.children);
+
 const btnNext = document.querySelector(".controls__btn--next");
 const btnPrev = document.querySelector(".controls__btn--prev");
+
+const carouselBtnNext = carouselContain.querySelector(".controls__btn--next");
+const carouselBtnPrev = carouselContain.querySelector(".controls__btn--prev");
 
 const catalogue = document.querySelector(".main__catalogue");
 const tabBtns = catalogue.querySelectorAll(".tabs__btn");
@@ -37,6 +44,9 @@ navInsertLink.addEventListener("click", showNavInsert);
 
 btnNext.addEventListener("click", showNewSlide);
 btnPrev.addEventListener("click", showPrevSlide);
+
+carouselBtnNext.addEventListener("click", showNextCarouselSlide);
+carouselBtnPrev.addEventListener("click", showPrevCarouselSlide);
 
 tabBtns.forEach(onTabClick);
 
@@ -132,7 +142,76 @@ function showPrevSlide(evt) {
 
 // sliderBottom
 
+carouselItems.forEach(function (item, index) {
 
+    // iterate over elements by index to add a class
+    if (index !== 0) {
+        item.classList.add("carousel__item--hidden")
+    } else { 
+        item.classList.remove("carousel__item--hidden");
+    };
+
+    // add data-index to html
+    item.dataset.index = index;
+
+    // add data-active for active element
+    carouselItems[0].setAttribute("data-active", "");
+
+    item.addEventListener("click", function () {
+        item.classList.add("carousel__item--hidden");
+        item.removeAttribute("data-active");
+
+        let nextItemIndex;
+        if (index + 1 === carouselItems.length) {
+            nextItemIndex = 0;
+        } else {
+            nextItemIndex = index + 1;
+        };
+
+        const nextCarouselItem = carousel.querySelector(`[data-index="${nextItemIndex}"]`);
+
+        nextCarouselItem.classList.remove("carousel__item--hidden");
+        nextCarouselItem.setAttribute("data-active", "");
+    });
+});
+
+function showNextCarouselSlide(evt) {
+    evt.preventDefault();
+    const currentItem = carousel.querySelector("[data-active]");
+    const currentItemIndex = +currentItem.dataset.index;
+    currentItem.classList.add("carousel__item--hidden");
+    currentItem.removeAttribute("data-active");
+
+    let nextItemIndex;
+    if (currentItemIndex + 1 === carouselItems.length) {
+        nextItemIndex = 0;
+    } else {
+        nextItemIndex = currentItemIndex + 1;
+    };
+
+    const nextItem = carousel.querySelector(`[data-index="${nextItemIndex}"]`);
+    nextItem.classList.remove("carousel__item--hidden");
+    nextItem.setAttribute("data-active", "");
+};
+
+function showPrevCarouselSlide(evt) {
+    evt.preventDefault();
+    const currentItem = carousel.querySelector("[data-active]");
+    const currentItemIndex = +currentItem.dataset.index;
+    currentItem.classList.add("carousel__item--hidden");
+    currentItem.removeAttribute("data-active");
+
+    let nextItemIndex;
+    if (currentItemIndex === 0) {
+        nextItemIndex = carouselItems.length - 1;
+    } else {
+        nextItemIndex = currentItemIndex - 1;
+    };
+
+    const nextItem = carousel.querySelector(`[data-index="${nextItemIndex}"]`);
+    nextItem.classList.remove("carousel__item--hidden");
+    nextItem.setAttribute("data-active", "");
+};
 
 // tab
 function onTabClick(item) {
